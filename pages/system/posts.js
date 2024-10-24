946aaf11e08234b62231fe6375dd4fb6f6ac0ed0
Ruan Emanuell Alencar <113607857+RuanEmanuell@users.noreply.github.com>
2024-10-24 13:39:39 -0300

    Fix: Minor fixes

--- pages/system/posts.js
+++ pages/system/posts.js
@@ -1,5 +1,17 @@
 verifyUser();
 
+function verifyUser(){
+    if(localStorage.getItem("loggedUser") == null){
+        document.location.href = "../../pages/login/login.html";
+    } 
+}
+
+function loggoutUser(){
+    localStorage.removeItem("loggedUser");
+    document.location.href = "../../pages/login/login.html";
+}
+
+
 let posts = [
     {
         userName: "Hemominas (Sede Juiz de Fora)",
--- pages/system/schedules.js
+++ pages/system/schedules.js
@@ -1,1 +1,12 @@
 verifyUser();
+
+function verifyUser(){
+    if(localStorage.getItem("loggedUser") == null){
+        document.location.href = "../../pages/login/login.html";
+    } 
+}
+
+function loggoutUser(){
+    localStorage.removeItem("loggedUser");
+    document.location.href = "../../pages/login/login.html";
+}
